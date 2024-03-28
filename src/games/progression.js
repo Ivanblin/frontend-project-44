27e0d5d4084e434152cd9bd915298d38cc8d0058
Кривов Иван <krivov_94@list.ru>
2024-03-28 21:02:13 +0400

    Рализация игры "Арифметическая прогрессия"

---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import { runEngine } from '../index.js';
+import { getRandomRange } from '../getRandomRange.js';
+
+const generateRandomProgression = (start, stepNumber, hiddenIndex) => {
+  // Создаем массив для хранения прогрессии
+  const progression = [];
+
+  // Заполняем массив числами прогрессии
+  for (let i = 0; i < 10; i++) {
+    progression.push(start + i * stepNumber);
+  }
+
+  const number = progression[hiddenIndex]
+  progression[hiddenIndex] = '...'
+
+  return [progression.join(' '), number];
+}
+
+const generateRound = () => {
+  const start = getRandomRange()
+  const stepNumber = getRandomRange(2, 10)
+  const hidden = getRandomRange(0, 9)
+  const [one, two] = generateRandomProgression(start, stepNumber, hidden)
+
+  const question = one
+  const answer = two
+
+  return [question, answer];
+}
+
+export const runProgressionGame = () => {
+  runEngine('Answer "yes" if the number is even, otherwise answer "no".', generateRound);
+}
